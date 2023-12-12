ea69ed1f129d21e60470d1d121e15838260e07a3
kachi <104820825+kachidavid@users.noreply.github.com>
2023-12-12 17:09:09 -0600

    Create name.py

---
+++ name.py
@@ -0,0 +1,27 @@
+def draw(card_rank):
+  if card_rank == 1:
+    # A 1 stands for an ace.
+    card_name = "Ace"
+  elif card_rank == 11:
+    # An 11 stands for a jack.
+    card_name = "Jack"
+  elif card_rank == 12:
+    # A 12 stands for a queen.
+    card_name = "Queen"
+  elif card_rank == 13:
+    # A 13 stands for a king.
+    card_name = "King"
+  else:
+    # All other cards are named by their
+    # number, or rank.
+    card_name = str(card_rank)
+
+  if card_rank == 1 or card_rank == 8:
+    drew_prefix = 'Drew an '
+  else:
+    drew_prefix = 'Drew a '
+
+  if card_rank < 1 or card_rank > 13:
+    return 'BAD CARD'
+  else:
+    return drew_prefix + card_name
